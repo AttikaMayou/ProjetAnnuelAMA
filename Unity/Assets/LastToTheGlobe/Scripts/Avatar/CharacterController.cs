390d67ec693941fef537f19246412fa48981389b
Margot <gotgot07@hotmail.fr>
2019-01-12 17:15:54 +0100

    Commit skeletal placeholder + material + textures + CharacterController et début CharacterControllerRigidbody

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Avatar/CharacterController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Auteur : Margot
+
+
+namespace LastToTheGlobe.Scripts.Dev
+{
+    public class CharacterController: MonoBehaviour
+    {
+        private CharacterController controller;
+
+        [SerializeField]
+        [Tooltip("Vitesse de la course")]
+        private float runSpeed;
+
+        [SerializeField]
+        [Tooltip("Vitesse de la marche")]
+        private float WalkSpeed;
+
+        [SerializeField]
+        [Tooltip("Gravité")]
+        public float gravity = 20.0F;
+
+        [SerializeField]
+        [Tooltip("Vitesse du saut")]
+        public float JumpSpeed;
+
+        private Animator animator;
+        // Deux floats pour faire avancer : Horizontal/Forward = walk devant derriere / Vertical/Strafe = walk gauche droite
+        private float Forward;
+        private float Strafe;
+        private Vector3 movement = Vector3.zero;
+        private float Speed= 1;
+
+        //CAMERA
+        [SerializeField]
+        private GameObject cameraRotatorX;
+
+        [SerializeField]
+        [Tooltip("Vitesse de la camera")]
+        private float rotationSpeed = 3;
+
+        void Start()
+        {
+
+            controller = GetComponent<CharacterController>();
+            animator = GetComponentInChildren<Animator>();
+
+            // character au sol
+            gameObject.transform.position = new Vector3(0, 0, 0);
+        }
+
+        void Update()
+        {   
+            //CAMERA
+            transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * rotationSpeed, 0));
+            cameraRotatorX.transform.Rotate(new Vector3(-(Input.GetAxis("Mouse Y") * rotationSpeed), 0, 0), Space.Self);
+
+            // Récupération des floats vertical et horizontal de l'animator au script
+            Forward = Input.GetAxis("Vertical");
+            Strafe = Input.GetAxis("Horizontal");
+
+            if (controller.isGrounded)
+            {
+                //Set movement
+                movement = new Vector3(Strafe, 0.0f, Forward);
+                movement = transform.TransformDirection(movement);
+
+                //Course
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    Speed = runSpeed; 
+
+                }
+                else
+                {
+                    Speed = WalkSpeed;
+                }                    
+                
+                //Saut
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    movement.y = JumpSpeed;
+                }
+                movement = movement * Speed;
+               
+            }       
+            //Ajout de la gravité
+            movement.y = movement.y - (gravity * Time.deltaTime);
+            //Move player with DeltaTime
+            controller.Move(movement * Time.deltaTime);
+        }
+    }
+}
---
+++ Unity/Assets/LastToTheGlobe/Scripts/Avatar/CharacterControllerRigidbody.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Auteur : Margot
+
+public class CharacterControllerRigidbody : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Vitesse de la course")]
+    private float runSpeed;
+
+    [SerializeField]
+    [Tooltip("Vitesse de la marche")]
+    private float walkSpeed;
+
+
+    [SerializeField]
+    [Tooltip("Character")]
+    public float body;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
