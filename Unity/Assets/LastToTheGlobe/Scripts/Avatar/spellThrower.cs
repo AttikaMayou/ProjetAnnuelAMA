02aff0d970ef39dfd4ccc5496e807e3632bdef6d
unknown <elhajjam.abdallah1@gmail.com>
2019-01-24 22:34:52 +0100

    Première version du système d'orb

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Avatar/spellThrower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spellThrower : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject testOrb;
+
+    private bool canThrowSpell;
+
+	// Use this for initialization
+	void Start () {
+        //testOrb = Resources.Load<GameObject>("testOrb");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A) && canThrowSpell)
+        {
+            canThrowSpell = false;
+            testOrb.SetActive(true);
+            
+
+        }
+        if (!testOrb.activeSelf)
+        {
+            canThrowSpell = true;
+        }
+    }
+}
---
+++ Unity/Assets/LastToTheGlobe/Scripts/Weapon/Orb/selfAttractorSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selfAttractorSetter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    private AttractorScript planetAttracting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        planetAttracting = player.GetComponent<AttractedScript>().attractor;
+        this.GetComponent<AttractedScript>().attractor = planetAttracting;
+    }
+}
