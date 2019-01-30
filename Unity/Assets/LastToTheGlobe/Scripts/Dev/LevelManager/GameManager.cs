4dd8957ee1c784567f8a489a2a3a01b664043491
Margot <gotgot07@hotmail.fr>
2019-01-30 23:02:07 +0100

    Ajout d'un script GameManager pour loader lobby + map

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Dev/LevelManager/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Auteur : Margot
+
+namespace LastToTheGlobe.Scripts.Dev
+{
+
+    public class GameManager : MonoBehaviourPunCallbacks
+    { 
+  
+        //Called when the local player left the room
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        //Load level lobby
+        public void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+
+            PhotonNetwork.LoadLevel("Lobby");
+
+        }
+
+
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion    
+    }
+
+}
