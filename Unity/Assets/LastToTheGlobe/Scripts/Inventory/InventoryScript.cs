cecaf1e4a92d65c7556a3fb08345bee46efdeba4
Attika <attika.mayou@gmail.com>
2019-01-31 14:21:26 +0100

    WIP Inventory

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Inventory/InventoryScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Photon.Pun.UtilityScripts;
+using UnityEngine;
+
+//Auteur : Attika
+
+namespace LastToTheGlobe.Scripts.Inventory
+{
+    public class InventoryScript : MonoBehaviour
+    {
+        public int nbSlots = 5;
+        public bool isFull;
+        public List<ObjectScript> objectsInInventory;
+
+
+        /// <summary>
+        /// Add an object in itself inventory
+        /// </summary>
+        /// <param name="obj"></param>
+        public void AddObjectInInventory(ObjectScript obj)
+        {
+            if(objectsInInventory.Contains(obj)) return;
+            objectsInInventory.Add(obj);
+            obj.SetObjectInInventory(true);
+            //TODO : add action in ObjectScript --> set object in inventory
+        }
+        
+        /// <summary>
+        /// Remove all objects from itself inventory
+        /// </summary>
+        public void EraseInventoryContent()
+        {
+            if (!isFull || objectsInInventory.Count <= 0) return;
+            foreach (var obj in objectsInInventory)
+            {
+                obj.SetObjectInInventory(false);
+                //TODO : add action in ObjectScript --> set object free
+            }
+            objectsInInventory.Clear();
+        }
+        
+        /// <summary>
+        /// Set the inventory status to full or not
+        /// </summary>
+        /// <returns></returns>
+        public void SetInventoryStatus()
+        {
+            isFull = objectsInInventory.Count > 0;
+           
+        }
+        
+        /// <summary>
+        /// Return true if inventory is full, false if not
+        /// </summary>
+        /// <returns></returns>
+        public bool IsInventoryFull()
+        {
+            return isFull;
+        }
+    }
+}
---
+++ Unity/Assets/LastToTheGlobe/Scripts/Inventory/ObjectScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+//Auteur : Attika
+
+namespace LastToTheGlobe.Scripts.Inventory
+{
+    public class ObjectScript : MonoBehaviour
+    {
+        public bool isInInventory;
+        public bool isConsume;
+        public int nbOfConsumption = 0;
+
+        /// <summary>
+        /// Set an object in inventory or delete it from inventory
+        /// </summary>
+        /// <param name="state"></param>
+        public void SetObjectInInventory(bool state)
+        {
+            //TODO : Refacto this function --> inventory will handle this kind of action so players cannot cheat
+            isInInventory = state;
+        }
+
+        /// <summary>
+        /// Return true if object is in inventory, false if not
+        /// </summary>
+        /// <returns></returns>
+        public bool IsObjectInInventory()
+        {
+            return isInInventory;
+        }
+        
+        /// <summary>
+        /// Set the status of an object to consumed or not
+        /// </summary>
+        /// <param name="state"></param>
+        public void SetConsumeStatus(bool state)
+        {
+            //TODO : Refacto this function --> inventory will handle this kind of action so players cannot cheat
+            isConsume = state;
+        }
+        
+        /// <summary>
+        /// Return true if object is already consume, false if not
+        /// </summary>
+        /// <returns></returns>
+        public bool IsObjectConsume()
+        {
+            return isConsume;
+        }
+    }
+}
