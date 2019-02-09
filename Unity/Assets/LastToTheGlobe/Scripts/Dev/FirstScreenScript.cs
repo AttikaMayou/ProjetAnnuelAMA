dbf10706351a05704f284f03ba2e39b26c75efd5
Attika <attika.mayou@gmail.com>
2019-02-09 11:17:50 +0100

    MasterScene update

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Dev/FirstScreenScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Auteur : Attika
+//Dirty script while we dont have a profile save management
+//This script is suppose to log on the player when he launch the game
+
+namespace LastToTheGlobe.Scripts.Dev
+{
+    public class FirstScreenScript : MonoBehaviour
+    {
+        public void SwitchToMainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
