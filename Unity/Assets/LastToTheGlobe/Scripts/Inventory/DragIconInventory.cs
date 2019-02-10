aea8c3a884885d9fe96ed003c0677a927cce3c91
Margot <gotgot07@hotmail.fr>
2019-02-10 19:02:59 +0100

    DragIcon bug

--- Unity/Assets/LastToTheGlobe/Scripts/Inventory/DragIconInventory.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Inventory/DragIconInventory.cs
@@ -9,7 +9,7 @@ using UnityEngine.EventSystems;
 public class DragIconInventory : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public static GameObject item;
-    private Vector3 startPosition;
+    private Vector3 startPosition = Vector3.zero;
     private Transform startParent;
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -17,24 +17,26 @@ public class DragIconInventory : MonoBehaviour, IBeginDragHandler, IDragHandler,
         item = gameObject;
         startPosition = transform.position;
         startParent = transform.parent;
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
-        transform.SetParent(transform.root);
+        //GetComponent<CanvasGroup>().blocksRaycasts = false;
+        //transform.SetParent(transform.root);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = Input.mousePosition;
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         item = null;
-        if(transform.parent == startParent || transform.parent == transform.root)
-        {
+        //GetComponent<CanvasGroup>().blocksRaycasts = true;
+        //if(transform.parent == startParent || transform.parent == transform.root)
+        //{
             transform.position = startPosition;
-            transform.SetParent(startParent);
-        }
+            //transform.SetParent(startParent);
+        //}
 
-        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
     }
 }
