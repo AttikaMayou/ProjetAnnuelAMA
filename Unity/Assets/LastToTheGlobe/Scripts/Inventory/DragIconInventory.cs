32ce3d895244266856c8b9e21b2118e31f996302
Margot <gotgot07@hotmail.fr>
2019-02-10 17:38:27 +0100

    UI inventory

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Inventory/DragIconInventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+//Auteur : Margot 
+
+public class DragIconInventory : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public static GameObject item;
+    Vector3 startPosition;
+    Transform startParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        item = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        item = null;
+
+        if(transform.parent != startParent)
+        {
+            transform.position = startPosition;
+        }
+
+    }
+}
