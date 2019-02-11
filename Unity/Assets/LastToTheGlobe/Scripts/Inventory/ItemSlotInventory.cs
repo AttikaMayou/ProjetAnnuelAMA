1878fa34b3f62df42569f2d1bc9b12bdffb1a721
Margot <gotgot07@hotmail.fr>
2019-02-11 22:20:37 +0100

    Environment GameRoom set for 2 players

--- Unity/Assets/LastToTheGlobe/Scripts/Inventory/ItemSlotInventory.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Inventory/ItemSlotInventory.cs
@@ -1,17 +1,24 @@
-﻿using System.Collections;
+﻿using LastToTheGlobe.Scripts.Singleton;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 //Auteur : Margot
 
-public class ItemSlotInventory : MonoBehaviour, IDropHandler
+public class ItemSlotInventory : MonoBehaviourSingleton<ItemSlotInventory>, IDropHandler
 {
+    [SerializeField]
+    private Button removeButton;
+    [SerializeField]
+    private Image removeButtonIcon;
+
     public GameObject item
     {
         get
         {
-            if(transform.childCount >0)
+            if(transform.childCount > 1)
             {
                 return transform.GetChild(0).gameObject;
             }
@@ -20,12 +27,36 @@ public class ItemSlotInventory : MonoBehaviour, IDropHandler
         }
     }
 
+    public void Awake()
+    {
+        removeButtonIcon.enabled = false;
+        removeButton.interactable = false;
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
+        //Ajout de l'icon dans le slot si vide
         if(!item)
         {
             DragIconInventory.item.transform.SetParent(transform);
+            removeButton.interactable = true;
+            removeButtonIcon.enabled = true;
         }
+    }
+
+    public void ClearSlot()
+    {
+        if(item)
+        {
+            removeButton.enabled = false;
+            removeButton.interactable = false;
+        }
+    }
+
+    public void OnRemoveButton()
+    {
+        Debug.Log("Press Remove Button");
+        //DragIconInventory._instance.Remove(item);
     }
 }
 
