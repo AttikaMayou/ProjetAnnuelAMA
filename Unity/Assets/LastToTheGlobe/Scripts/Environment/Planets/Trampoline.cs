a5eac09d35e55fb8c6a48b317d099dc7384ca1d0
Margot <gotgot07@hotmail.fr>
2019-02-20 23:52:19 +0100

    trampoline script 2

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Environment/Planets/Trampoline.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LastToTheGlobe.Scripts.Avatar;
+
+public class Trampoline : MonoBehaviour
+{
+    [SerializeField] private CharacterExposer playerExposer;
+    public KeyCode defensiveOrbInput;
+    private bool _canHyperJump;
+    private float _cooldownFinished = 0.0f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(defensiveOrbInput) && _canHyperJump && _cooldownFinished <= 0f)
+        {
+            _cooldownFinished = 10f;
+            playerExposer.characterRb.AddForce(transform.up * 1300f);
+        }
+
+        if (_cooldownFinished >= 0f)
+        {
+            _cooldownFinished -= Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Jumper"))
+        {
+            _canHyperJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Jumper"))
+        {
+            _canHyperJump = false;
+        }
+
+    }
+}
