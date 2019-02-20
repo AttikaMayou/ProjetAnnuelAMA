6e908e25b0d6b63bcfe859b5629cd10c728735df
Margot <gotgot07@hotmail.fr>
2019-02-20 22:58:15 +0100

    Add Ui victory et defeat

---
+++ Unity/Assets/LastToTheGlobe/Scripts/UI/UIVictory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Auteur: Margot
+
+public class UIVictory : MonoBehaviour
+{
+    [SerializeField] private Canvas victory;
+    [SerializeField] private Canvas defeat;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        victory.enabled = false;
+        defeat.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
