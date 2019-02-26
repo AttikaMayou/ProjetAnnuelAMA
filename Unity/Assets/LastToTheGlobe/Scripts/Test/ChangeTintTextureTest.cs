65e4c650dccb149a0a5baaf74d86af9d724f4009
Margot <gotgot07@hotmail.fr>
2019-02-26 23:05:26 +0100

    Change color material character

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Test/ChangeTintTextureTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTintTextureTest : MonoBehaviour
+{
+    private Renderer rend;
+    [SerializeField]
+    private Color color = Color.white;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = color;
+    }
+
+}
