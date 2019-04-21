21761e97a18276b9d77bad266f5e54ef85401430
Margot <gotgot07@hotmail.fr>
2019-04-21 19:17:02 +0200

    instanciation coffre et tremplin

--- Unity/Assets/LastToTheGlobe/Scripts/Environment/ProceduralGenerationMap/Planet/AssetInstanciation.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Environment/ProceduralGenerationMap/Planet/AssetInstanciation.cs
@@ -7,12 +7,6 @@ using UnityEngine;
 public class AssetInstanciation : MonoBehaviour
 {
     [SerializeField]
-    private int numberObjectMax = 1;
-
-    [SerializeField]
-    private int numberTreesMax;
-
-    [SerializeField]
     private GameObject planet;
 
     [SerializeField]
@@ -33,6 +27,9 @@ public class AssetInstanciation : MonoBehaviour
     [SerializeField]
     private List<GameObject> desertRock;
 
+    private int numberObjectMax = 1;
+    private int numberTreesMax;
+    private int maxObject;
     private List<GameObject> listTrees;
     private List<GameObject> listRock;
     private float randomScaleTree;
@@ -41,6 +38,8 @@ public class AssetInstanciation : MonoBehaviour
     void Start()
     {
         Vector3 planetPosition = gameObject.transform.position;
+        numberObjectMax = (int)Random.Range(1, 200);
+        numberTreesMax = (int)Random.Range(0, 50);
 
         if (planet.CompareTag("frozen"))
         {
@@ -58,14 +57,14 @@ public class AssetInstanciation : MonoBehaviour
             listRock = basicRock;
         }
 
-        if (numberTreesMax > 0 && numberObjectMax >0)
+        if (numberTreesMax > 0)
         {
             //Spawn Trees
             for (int i = 0; i <= numberTreesMax; i++)
             {
                 randomScaleTree = Random.Range(0.05f, 0.15f);
 
-                Vector3 spawnPosition = Random.onUnitSphere * ((gameObject.transform.localScale.x / 2) + listTrees[0].transform.localScale.y - 0.02f) + planet.transform.position;
+                Vector3 spawnPosition = Random.onUnitSphere * ((planet.transform.localScale.x / 2) + listTrees[0].transform.localScale.y - 0.02f) + planet.transform.position;
                 GameObject newtree = Instantiate(listTrees[GetRandomTree()], spawnPosition, Quaternion.identity) as GameObject;
 
                 newtree.transform.LookAt(planetPosition);
@@ -75,22 +74,20 @@ public class AssetInstanciation : MonoBehaviour
             }
         }
 
-        if (numberObjectMax > 0)
+        //Spawn Rock
+        for (int i = numberTreesMax; i < numberObjectMax; i++)
         {
-            //Spawn Rock
-            for (int i = numberTreesMax; i < numberObjectMax; i++)
-            {
-                randomScaleRock = Random.Range(0.05f, 0.1f);
+            randomScaleRock = Random.Range(0.02f, 0.06f);
 
-                Vector3 spawnPosition = Random.onUnitSphere * ((planet.transform.localScale.x / 2) + listRock[0].transform.localScale.y - 0.05f) + planet.transform.position;
-                GameObject newrock = Instantiate(listRock[GetRandomRock()], spawnPosition, Quaternion.identity) as GameObject;
+            Vector3 spawnPosition = Random.onUnitSphere * ((planet.transform.localScale.x / 2) + listRock[0].transform.localScale.y - 0.05f) + planet.transform.position;
+            GameObject newrock = Instantiate(listRock[GetRandomRock()], spawnPosition, Quaternion.identity) as GameObject;
 
-                newrock.transform.LookAt(planetPosition);
-                newrock.transform.localScale = new Vector3(randomScaleRock, randomScaleRock, randomScaleRock);
-                newrock.transform.Rotate(-90, 0, 0);
-                newrock.transform.parent = transform;
-            }
+            newrock.transform.LookAt(planetPosition);
+            newrock.transform.localScale = new Vector3(randomScaleRock, randomScaleRock, randomScaleRock);
+            newrock.transform.Rotate(-90, 0, 0);
+            newrock.transform.parent = transform;
         }
+        
     }
 
     private int GetRandomTree()
---
+++ Unity/Assets/LastToTheGlobe/Scripts/Environment/ProceduralGenerationMap/Planet/GPInstanciation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPInstanciation : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tremplin;
+
+    [SerializeField]
+    private GameObject chest;
+
+    [SerializeField]
+    private GameObject planet;
+
+    private int numberOfTremplin;
+    private int numberOfChest;
+
+    void Start()
+    {
+        numberOfTremplin = Random.Range(1, 5);
+        numberOfChest = Random.Range(1, 3);
+        //TODO : changer en fonction de la taille des planètes
+        // faire en sorte qu'ils soient toujours vers d'autres plantètes
+
+        for (int i = 0; i <= numberOfTremplin; i++)
+        {
+            Vector3 planetPosition = gameObject.transform.position;
+
+            Vector3 spawnPosition = Random.onUnitSphere * ((planet.transform.localScale.x / 2) + tremplin.transform.localScale.y - 0.02f) + planet.transform.position;
+            GameObject newtree = Instantiate(tremplin, spawnPosition, Quaternion.identity) as GameObject;
+
+            newtree.transform.LookAt(planetPosition);
+            newtree.transform.Rotate(-90, 0, 0);
+            newtree.transform.parent = transform;
+        }
+
+        for (int i = 0; i <= numberOfChest; i++)
+        {
+            Vector3 planetPosition = gameObject.transform.position;
+
+            Vector3 spawnPosition = Random.onUnitSphere * ((planet.transform.localScale.x / 2) + chest.transform.localScale.y - 0.02f) + planet.transform.position;
+            GameObject newtree = Instantiate(chest, spawnPosition, Quaternion.identity) as GameObject;
+
+            newtree.transform.LookAt(planetPosition);
+            newtree.transform.Rotate(-90, 0, 0);
+            newtree.transform.parent = transform;
+        }
+    }
+
+
+}
