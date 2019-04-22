150519ff38c5636bfa8ab4a1de0975b7385ed627
Margot <gotgot07@hotmail.fr>
2019-04-22 20:54:05 +0200

    life

--- Unity/Assets/LastToTheGlobe/Scripts/Avatar/AvatarLifeManager.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Avatar/AvatarLifeManager.cs
@@ -1,18 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
 
 //Auteur : Attika
+//modification : Margot
 
 namespace LastToTheGlobe.Scripts.Avatar
 {
     public class AvatarLifeManager : MonoBehaviour
     {
+        [SerializeField] private Canvas defeat;
+
         [Header("Balance Settings")] 
-        public int lifeStartingPoint;
+        public int lifeStartingPoint = 100;
         public int attackDmg;
+
+        public bool inLife = true;
         
-        public int myLife;
+        public int myLife = 100;
         public CharacterExposer myExposer;
-        
+
+        [SerializeField]
+        private Text textHealth;
+
+        [SerializeField]
+        private Text textHeathOther;
+
+        private void Start()
+        {
+            defeat.enabled = false;
+        }
+
         private void Awake()
         {
             myLife = lifeStartingPoint;
@@ -20,17 +39,53 @@ namespace LastToTheGlobe.Scripts.Avatar
 
         private void OnCollisionEnter(Collision other)
         {
-            if (!other.gameObject.CompareTag("Bullet")) return;
-            //if (other.gameObject == myExposer.thirdPersonController.orb) return;
-            myLife -= attackDmg;
+            if (other.gameObject.tag == "Bullet" && myExposer.characterLocalPhotonView.IsMine)
+            {
+                if(myLife <= 0)
+                {
+                    textHealth.text = "Health : 0";
+
+                }
+                else
+                {
+                    myLife -= attackDmg;
+                    textHealth.text = "Health :" + myLife;
+                }
+
+                myExposer.characterLocalPhotonView.RPC("MajMine", RpcTarget.Others, myLife);
+            }
+            else
+            {
+                return;
+            }
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
+            if(Input.GetKeyDown(KeyCode.P))
+            {
+                if (myLife <= 0)
+                {
+                    textHealth.text = "Health : 0";
+                }
+                else
+                {
+                    myLife -= 10;
+                    textHealth.text = "Health :" + myLife;
+                }
+            }
+
             if (myLife <= 0)
             {
-                
+                inLife = false;
+                defeat.enabled = true;
             }
+        }
+
+        [PunRPC]
+        void MajMine()
+        {
+            textHeathOther.text = "Health :" + myLife;
         }
     }
 }
