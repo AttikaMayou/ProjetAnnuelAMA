8bd5b7a4e314909b5138eeb348e96235636ba6c6
unknown <elhajjam.abdallah1@gmail.com>
2019-04-22 23:38:23 +0200

    Fixing Camera v2

--- Unity/Assets/LastToTheGlobe/Scripts/Avatar/ThirdPersonScript.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Avatar/ThirdPersonScript.cs
@@ -15,23 +15,33 @@ namespace LastToTheGlobe.Scripts.Avatar
         [SerializeField]
         private float rotationSpeed = 5.0f;
         private Quaternion _rotation;
+        private Vector3 _moveDir;
+        public float speed = 5f;
 
         private void FixedUpdate()
         {
+            
+            _moveDir = new Vector3(Input.GetAxisRaw("Horizontal"),
+                0,
+                Input.GetAxisRaw("Vertical")).normalized;
+        
+            playerExposer.characterRb.MovePosition(playerExposer.characterRb.position + transform.TransformDirection(_moveDir) * speed * Time.deltaTime);
+            //Permet de tourner le personnage pour donner la direction à la caméra sur l'axe horizontale
             transform.Rotate(new Vector3(0,
                 Input.GetAxis("Mouse X") * rotationSpeed,
                 0));
         
         
-            //Rotate cameraRotatorX to give the camera the right vertical axe
+            //Permet de tourner le gameobject qui donne la direction à la caméra sur l'axe vertical
             playerExposer.cameraRotatorX.transform.Rotate(new Vector3(-(Input.GetAxis("Mouse Y") * rotationSpeed),
                 0,
                 0), Space.Self);
+
         
         
             //Prevent the camera from going too high or too low
             //Les valeurs qui étaient mises sont des valeurs qui peuvent être pris par la variable cameraRotatorX.transform.rotation.x (-1 - 1)
-            if (playerExposer.cameraRotatorX.transform.rotation.x >= 0.42f)
+            /*if (playerExposer.cameraRotatorX.transform.rotation.x >= 0.42f)
             {
                 _rotation = 
                     new Quaternion(0.42f, _rotation.y, 
@@ -45,7 +55,7 @@ namespace LastToTheGlobe.Scripts.Avatar
                     new Quaternion(-0.2f, _rotation.y, 
                         _rotation.z, _rotation.w);
                 playerExposer.cameraRotatorX.transform.rotation = _rotation;
-            }
+            }*/
         }
     }
 }
