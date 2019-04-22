34418526c70428c05745e8f846c4a5e003bf1e0b
Attika <attika.mayou@gmail.com>
2019-04-22 16:31:39 +0200

    WIP Clean GameRoom + SunController

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Environment/SunController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+//Auteur : Attika
+
+namespace LastToTheGlobe.Scripts.Environment
+{
+    public class SunController : MonoBehaviour
+    {
+        [SerializeField] private Vector3 startPos;
+        
+        private void Awake()
+        {
+            gameObject.transform.position = startPos;
+        }
+        
+        //TODO : add methods for sun control (UI feedback for players + movement of the object + lighting setup)
+    }
+}
