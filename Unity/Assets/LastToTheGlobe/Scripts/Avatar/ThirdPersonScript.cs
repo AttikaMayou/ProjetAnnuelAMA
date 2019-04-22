a22c7231b6579334ee220895ff39ac047157bf85
Attika <attika.mayou@gmail.com>
2019-04-22 22:21:06 +0200

    WIP shoot + inventory + camera rotation

---
+++ Unity/Assets/LastToTheGlobe/Scripts/Avatar/ThirdPersonScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+//Auteur : Attika
+//Modifications : Abdallah
+
+namespace LastToTheGlobe.Scripts.Avatar
+{
+    public class ThirdPersonScript : MonoBehaviour
+    {
+        [SerializeField] private CharacterExposer exposer;
+        
+    }
+}
