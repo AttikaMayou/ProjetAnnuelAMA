e401a123d3b46edff8bab3d5aca2dad687f7593e
Attika <attika.mayou@gmail.com>
2019-04-23 00:10:18 +0200

    Fix Camera

--- Unity/Assets/LastToTheGlobe/Scripts/Avatar/ThirdPersonScript.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Avatar/ThirdPersonScript.cs
@@ -9,36 +9,27 @@ namespace LastToTheGlobe.Scripts.Avatar
 {
     public class ThirdPersonScript : MonoBehaviour
     {
+        public bool debug = true;
+        
         [SerializeField] private CharacterExposerScript playerExposer;
         [Header("Camera Parameters")]
-        public CameraScript myCamera;
         [SerializeField]
         private float rotationSpeed = 5.0f;
         private Quaternion _rotation;
-        private Vector3 _moveDir;
         public float speed = 5f;
 
         private void FixedUpdate()
         {
-            
-            _moveDir = new Vector3(Input.GetAxisRaw("Horizontal"),
-                0,
-                Input.GetAxisRaw("Vertical")).normalized;
-        
-            playerExposer.characterRb.MovePosition(playerExposer.characterRb.position + transform.TransformDirection(_moveDir) * speed * Time.deltaTime);
             //Permet de tourner le personnage pour donner la direction à la caméra sur l'axe horizontale
             transform.Rotate(new Vector3(0,
                 Input.GetAxis("Mouse X") * rotationSpeed,
                 0));
-        
         
             //Permet de tourner le gameobject qui donne la direction à la caméra sur l'axe vertical
             playerExposer.cameraRotatorX.transform.Rotate(new Vector3(-(Input.GetAxis("Mouse Y") * rotationSpeed),
                 0,
                 0), Space.Self);
 
-        
-        
             //Prevent the camera from going too high or too low
             //Les valeurs qui étaient mises sont des valeurs qui peuvent être pris par la variable cameraRotatorX.transform.rotation.x (-1 - 1)
             /*if (playerExposer.cameraRotatorX.transform.rotation.x >= 0.42f)
