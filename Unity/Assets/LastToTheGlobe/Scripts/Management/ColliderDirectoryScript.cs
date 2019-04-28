fd4a44d871bc042c23ca01ea7735abaf69de36d3
Attika <attika.mayou@gmail.com>
2019-04-28 17:29:12 +0200

    Add planets methods in directory script

--- Unity/Assets/LastToTheGlobe/Scripts/Management/ColliderDirectoryScript.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Management/ColliderDirectoryScript.cs
@@ -14,12 +14,15 @@ namespace LastToTheGlobe.Scripts.Management
     {
         public bool debug = true;
         public List<CharacterExposerScript> characterExposers;
+        public List<PlanetExposerScript> planetExposers;
         
         private Dictionary<Collider, CharacterExposerScript> _playersDirectory = new Dictionary<Collider, CharacterExposerScript>();
         private CharacterExposerScript _playerValue;
         
         private Dictionary<Collider, PlanetExposerScript> _planetsDirectory = new Dictionary<Collider, PlanetExposerScript>();
+        private PlanetExposerScript _planetValue;
 
+        #region Players Methods
         //Get the player whom belongs to the collider
         public CharacterExposerScript GetCharacterExposer(Collider col)
         {
@@ -39,6 +42,7 @@ namespace LastToTheGlobe.Scripts.Management
             {
                 characterExposers.Add(player);
             }
+            
             AddPlayerInDirectory(player);
         }
 
@@ -49,6 +53,41 @@ namespace LastToTheGlobe.Scripts.Management
             _playersDirectory.Add(player.characterCollider, player);
             if(debug) Debug.Log("Directory key : " + player.characterCollider + " and value : " + player);
         }
+        #endregion
+        
+        #region Planets Methods
+
+        public PlanetExposerScript GetPlanetExposerScript(Collider col)
+        {
+            if(debug) Debug.Log("trying to find planet from this collider : " + col);
+            if (!PhotonNetwork.IsMasterClient) return null;
+            return _planetsDirectory.TryGetValue(col, out _planetValue) ? _planetValue : null;
+        }
+
+        public void AddPlanetExposer(PlanetExposerScript planet)
+        {
+            if (planetExposers == null)
+            {
+                planetExposers = new List<PlanetExposerScript>();
+            }
+
+            if (!planetExposers.Contains(planet) && planet)
+            {
+                planetExposers.Add(planet);
+            }
+
+            AddPlanetInDirectory(planet);
+        }
+
+        private void AddPlanetInDirectory(PlanetExposerScript planet)
+        {
+            if(debug) Debug.Log("add one planet to directory");
+            if (_planetsDirectory.ContainsValue(planet)) return;
+            _planetsDirectory.Add(planet.planetCollider, planet);
+            if(debug) Debug.Log("Directory key : " + planet.planetCollider + " and value : " + planet);
+        }
+        
+        #endregion
 
     }
 }
