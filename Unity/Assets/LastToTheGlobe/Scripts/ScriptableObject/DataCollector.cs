a7949143c3c694cf818d0578a7f59433e9e64aa9
Margot <gotgot07@hotmail.fr>
2019-06-01 15:14:13 +0200

    Scriptable Object

---
+++ Unity/Assets/LastToTheGlobe/Scripts/ScriptableObject/DataCollector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollector : MonoBehaviour
+{
+    private static DataCollector instance;
+
+    [SerializeField] private KillingDataListScript dataVault;
+    [SerializeField] private bool resetData = true;
+
+    public static DataCollector Instance()
+    {
+        return instance;
+    }
+
+    private void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        if (this.resetData)
+        {
+            instance.dataVault.ResetData();
+        }
+            
+    }
+    /*
+    public static void RegisterMinionKill(MinionBehaviour m)
+    {
+        if (instance != null && instance.dataVault != null) instance.dataVault.AddKillPosEntry(m.transform.position);
+    }
+    public static void RegisterMinionKillWithTime(MinionBehaviour m)
+    {
+        if (instance != null && instance.dataVault != null) instance.dataVault.AddKillPosEntry(m.transform.position, Time.time);
+    }
+    */
+}
---
+++ Unity/Assets/LastToTheGlobe/Scripts/ScriptableObject/KillingDataListScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "KillingDataListScript", menuName = "ScriptableData", order = 1)]
+public class KillingDataListScript : ScriptableObject
+{
+    [SerializeField]
+    private List<KillData> killingDataList;
+
+    public List<KillData> publickillingDataList => this.killingDataList;
+
+    public void ResetData()
+    {
+        this.killingDataList.Clear();
+    }
+
+    public void AddKillPosEntry(Vector3 pos)
+    {
+        this.killingDataList.Add(new KillData() { killPosition = pos });
+    }
+    public void AddKillPosEntry(Vector3 pos, float timeStamp)
+    {
+        this.killingDataList.Add(new KillData() { killPosition = pos, killTime = timeStamp });
+    }
+
+    public void AddPlayerKill(KillData data)
+    {
+        this.killingDataList.Add(data);
+    }
+}
+    [Serializable]
+    public class KillData
+    {
+        public Vector3 killPosition;
+        public float killTime;
+        public PlanetType killOnTypePlanet;
+    }
