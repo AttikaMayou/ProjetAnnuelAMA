1aa326fdedce6ac8b48043607cdcbcfb15303af4
EBElder <elhajjam.abdallah1@gmail.com>
2019-06-23 13:01:16 +0200

    Trying to make a input fiel sending request

---
+++ Unity/Assets/LastToTheGlobe/Scripts/UI/UIConnexion/Send.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Send : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    
+    public void SendReq(string text)
+    {
+        print("Yass");
+    }
+}
---
+++ Unity/Assets/LastToTheGlobe/Scripts/httpRequests/httpRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class httpRequest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Request());
+    }
+
+    // Update is called once per frame
+    IEnumerator Request()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("Username","EBElder", Encoding.UTF8);
+        form.AddField("Password","EBElder", Encoding.UTF8);
+        UnityWebRequest www = UnityWebRequest.Post("https://ebelder.pythonanywhere.com/login", form);
+        yield return www.SendWebRequest();
+        
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(Request());
+        }
+    }
+}
