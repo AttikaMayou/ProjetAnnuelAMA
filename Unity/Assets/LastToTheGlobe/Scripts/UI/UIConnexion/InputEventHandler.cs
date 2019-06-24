274a0f505ebea809ba3894737238f0dedb5f28f4
elhajjam.abdallah1@gmail.com <elhajjam.abdallah1@gmail.com>
2019-06-24 21:07:43 +0200

    WIP Trying to make a input fiel sending request v3

---
+++ Unity/Assets/LastToTheGlobe/Scripts/UI/UIConnexion/InputEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class InputEventHandler : MonoBehaviour
+{
+    public TMP_InputField mainInputField;
+    [HideInInspector]
+    public bool inputEntered;
+
+    // Checks if there is anything entered into the input field.
+    void LockInput(TMP_InputField input)
+    {
+        if (input.text.Length > 0)
+        {
+            inputEntered = true;
+        }
+        else if (input.text.Length == 0)
+        {
+            Debug.Log("Main Input Empty");
+        }
+    }
+
+    public void Start()
+    {
+        //Adds a listener that invokes the "LockInput" method when the player finishes editing the main input field.
+        //Passes the main input field into the method when "LockInput" is invoked
+        mainInputField.onEndEdit.AddListener(delegate {LockInput(mainInputField); });
+    }
+}
---
+++ Unity/Assets/LastToTheGlobe/Scripts/UI/UIConnexion/SendRequest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SendRequest : MonoBehaviour {
+    public Button yourButton;
+
+    public GameObject pseudo;
+    public GameObject mdp;
+    public bool boolPseudo;
+    public bool boolMdp;
+    
+    void Start () {
+        yourButton.onClick.AddListener(TaskOnClick);
+        boolPseudo = pseudo.GetComponent<InputEventHandler>().mainInputField;
+        boolMdp = mdp.GetComponent<InputEventHandler>().mainInputField;
+    }
+
+    void TaskOnClick()
+    {
+        if (boolPseudo && boolMdp)
+        {
+            Debug.Log("Requete envoyé!");    
+        }
+        else
+        {
+            Debug.Log("Remplissez les champs");
+        }
+        
+        
+    }
+}
