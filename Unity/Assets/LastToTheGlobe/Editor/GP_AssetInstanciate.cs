5eadcde62e9e64ee20006f7096b4cb8823f849ef
Margot <gotgot07@hotmail.fr>
2019-06-26 23:37:43 +0200

    Problème de nullref quand la fonction d'instanciation est dans un autre script

---
+++ Unity/Assets/LastToTheGlobe/Editor/GP_AssetInstanciate.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Random = UnityEngine.Random;
+
+//Auteur: Margot
+//Modifications : Attika
+
+namespace LastToTheGlobe.Editor
+{
+    public class GP_AssetInstanciate : MonoBehaviour
+    {
+
+        [SerializeField]
+        private List<GameObject> basicTrees;
+
+        [SerializeField]
+        private List<GameObject> basicRock;
+
+        [SerializeField]
+        private List<GameObject> frozenTrees;
+
+        [SerializeField]
+        private List<GameObject> frozenRock;
+
+        [SerializeField]
+        private List<GameObject> desertTrees;
+
+        [SerializeField]
+        private List<GameObject> desertRock;
+
+
+        private GameObject newTree;
+        private int randomBasicTrees;
+        private int _numberObjectMax = 1;
+        private int _numberTreesMax;
+        private int _maxObject;
+        private List<GameObject> _listTrees;
+        private List<GameObject> _listRock;
+        private float _randomScaleTree;
+        private float _randomScaleRock;
+
+        public void CreateAssets(int numberOfTrees, int numberOfRock, PlanetType type, GameObject planet)
+        {
+            randomBasicTrees = Random.Range(1, 4);
+            var planetPosition = gameObject.transform.position;
+            _numberObjectMax = (int)Random.Range(1, 200);
+            _numberTreesMax = (int)Random.Range(0, 50);
+
+            switch ((PlanetType)type)
+            {
+                case PlanetType.Frozen:
+                    _listTrees = frozenTrees;
+                    _listRock = frozenRock;
+                    break;
+                case PlanetType.Desert:
+                    _listTrees = desertTrees;
+                    _listRock = desertRock;
+                    break;
+                case PlanetType.Basic:
+                    _listTrees = basicTrees;
+                    _listRock = basicRock;
+                    break;
+                default:
+                    _listTrees = basicTrees;
+                    _listRock = basicRock;
+                    break;
+            }
+
+            Debug.Log("type dans instanciate = " + type);
+            if (numberOfTrees > 0)
+            {
+                //Spawn Trees
+                for (int i = 1; i <= numberOfTrees; i++)
+                {
+                    _randomScaleTree = Random.Range(0.05f, 0.15f);
+
+                    newTree = Instantiate(Resources.Load("SM_Basic_Tree_0" + randomBasicTrees, typeof(GameObject))) as GameObject;
+                    var spawnPosition = Random.onUnitSphere * (((planet.transform.localScale.x / 2) - 0.2f) + newTree.transform.localScale.y - 0.1f) + planet.transform.position;
+                    newTree.transform.position = spawnPosition;
+                    newTree.transform.LookAt(planet.transform.position);
+                    newTree.transform.localScale = new Vector3(_randomScaleTree, _randomScaleTree, _randomScaleTree);
+                    newTree.transform.Rotate(-90, 0, 0);
+                    //newTree.transform.parent = transform;
+                }
+            }
+
+            //Spawn Rock
+            for (var i = _numberTreesMax; i < _numberObjectMax; i++)
+            {
+                _randomScaleRock = Random.Range(0.02f, 0.06f);
+
+                var spawnPosition = Random.onUnitSphere * ((planet.transform.localScale.x / 2) + _listRock[0].transform.localScale.y - 0.05f) + planet.transform.position;
+                var newRock = PhotonNetwork.Instantiate(_listRock[GetRandomRock(type)].name, spawnPosition, Quaternion.identity) as GameObject;
+
+                newRock.transform.LookAt(planetPosition);
+                newRock.transform.localScale = new Vector3(_randomScaleRock, _randomScaleRock, _randomScaleRock);
+                newRock.transform.Rotate(-90, 0, 0);
+                newRock.transform.parent = transform;
+            }
+        }
+
+        private int GetRandomTree(PlanetType type)
+        {
+            switch ((PlanetType)type)
+            {
+                case PlanetType.Frozen:
+                    return (int)Random.Range(0, frozenTrees.Count - 1);
+                case PlanetType.Desert:
+                    return (int)Random.Range(0, desertTrees.Count - 1);
+                case PlanetType.Basic:
+                    break;
+                default:
+                    return (int)Random.Range(0, basicTrees.Count - 1);
+            }
+
+            return 0;
+        }
+
+        private int GetRandomRock(PlanetType type)
+        {
+            switch ((PlanetType)type)
+            {
+                case PlanetType.Frozen:
+                    return (int)Random.Range(0, frozenRock.Count - 1);
+                case PlanetType.Desert:
+                    return (int)Random.Range(0, desertRock.Count - 1);
+                case PlanetType.Basic:
+                    break;
+                default:
+                    return (int)Random.Range(0, basicRock.Count - 1);
+            }
+
+            return 0;
+        }
+
+    }
+}
--- Unity/Assets/LastToTheGlobe/Editor/GP_GUI.cs
+++ Unity/Assets/LastToTheGlobe/Editor/GP_GUI.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using LastToTheGlobe.Scripts.Environment.ProceduralGenerationMap.Planet;
-
+using LastToTheGlobe.Editor;
 
 //Auteur : Margot
 namespace Editor
@@ -11,11 +10,13 @@ namespace Editor
     public class GP_GUI : EditorWindow
     {
         public GP_GUISettings GUISettings { get; set; }
-        private List<GameObject> forPrefab;
+        private List<GameObject> forPrefab = new List<GameObject>();
         private GameObject planet;
         private Material mat;
         private GameObject spawnPlanet;
-        private AssetInstanciation_PUN spawnAsset;
+        private GP_AssetInstanciate spawnAsset;
+        private GameObject newTree;
+        private int randomBasicTrees;
 
         public void OnGUI()
         {
@@ -75,17 +76,16 @@ namespace Editor
             if(GUISettings.SpawnPlanet == true)
             {
                 spawnPlanet = Instantiate(Resources.Load("SM_SpawnPlanet", typeof(GameObject))) as GameObject;
-                forPrefab[0] = spawnPlanet;
+                forPrefab.Insert(0, planet);
             }
             else
             {
                 //Create planet sphere and set scale
                 planet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                forPrefab.Add(planet);
+                forPrefab.Insert(0, planet);
                 Undo.RegisterCreatedObjectUndo(planet, "Create planets");
                 //SCALE
                 planet.transform.localScale = new Vector3(GUISettings.Scale, GUISettings.Scale, GUISettings.Scale);
-
                 //----------------------------------PLANET TYPE---------------------------------------------------
                 switch (GUISettings.PlanetType)
                 {
@@ -111,13 +111,31 @@ namespace Editor
                 }
                 //MATERIAL
                 planet.GetComponent<Renderer>().material = mat;
+/*
+                randomBasicTrees = Random.Range(1, 4);
+
+                for (int i = 1; i <= GUISettings.NumberOfTree; i++)
+                {
+                    //_randomScaleTree = Random.Range(0.05f, 0.15f)
+                    
+                    newTree = Instantiate(Resources.Load("SM_Basic_Tree_0" + randomBasicTrees, typeof(GameObject))) as GameObject;
+                    var spawnPosition = Random.onUnitSphere * (((planet.transform.localScale.x / 2) - 0.2f) + newTree.transform.localScale.y - 0.1f) + planet.transform.position;
+                    newTree.transform.position = spawnPosition;
+                    //newTree.transform.position = new Vector3(0, 0, 0);
+                    newTree.transform.LookAt(planet.transform.position);
+                    //newTree.transform.localScale = new Vector3(_randomScaleTree, _randomScaleTree, _randomScaleTree);
+                    newTree.transform.Rotate(-90, 0, 0);
+                    //newTree.transform.parent = transform;
+                }*/
             }
             //SpawnObject
-            //TODO : faire une liste des games objects avec instantiate resources load
-            //spawnAsset.SpawnAssets(GUISettings.NumberOfTree, GUISettings.NumberOfRock, GUISettings.PlanetType, planet);
+            spawnAsset.CreateAssets(GUISettings.NumberOfTree, GUISettings.NumberOfRock, GUISettings.PlanetType, planet);
+
+            //Spawn Trees
+            
         }
 
-        
+
         public void CreatePrefab()
         {
             foreach (GameObject gameObject in forPrefab)
@@ -125,7 +143,6 @@ namespace Editor
                 //Set the path as within the ressources folder
                 string localPath = "Assets/Resources/" + GUISettings.Name + ".prefab";
                 CreateNew(gameObject, localPath);
-           
             }
         }
         
@@ -139,8 +156,12 @@ namespace Editor
         //reset object
         public void ResetScene()
         {
+            string list = forPrefab[0].name;
+            Debug.Log("reset");
+            Debug.Log(forPrefab[0].name);
             DestroyImmediate(forPrefab[0]);
             forPrefab.Clear();
+            Debug.Log("liste après: " + list);
             //TODO : vérifier la liste vidée
         }
        
