49a724b2aa9fb81ae77c59c2ae1297c0629b166e
Attika <attika.mayou@gmail.com>
2019-07-11 19:04:11 +0200

    WIP Defensive Orb

--- Unity/Assets/LastToTheGlobe/Scripts/Weapon/Orb/OrbDefensiveScript.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Weapon/Orb/OrbDefensiveScript.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using LastToTheGlobe.Scripts.Avatar;
+using LastToTheGlobe.Scripts.Management;
+using UnityEngine;
 
 //Auteur : Attika
 
@@ -6,6 +9,43 @@ namespace LastToTheGlobe.Scripts.Weapon.Orb
 {
     public class OrbDefensiveScript : MonoBehaviour
     {
+        [SerializeField] private CharacterExposerScript player;
+        [SerializeField] private Vector3 initialPosition;
+        [SerializeField] private Quaternion initialRotation;
+
+        private float _timeSpend;
+        private Transform _transform;
         
+        private void OnEnable()
+        {
+            if (!player)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+            _timeSpend = 0.0f;
+            _transform = transform;
+            initialPosition = _transform.localPosition;
+            initialRotation = _transform.localRotation;
+        }
+
+        private void FixedUpdate()
+        {
+            _timeSpend += Time.deltaTime;
+            _timeSpend %= (Mathf.PI * 2);
+
+            _transform.RotateAround(player.CharacterTr.position, player.CharacterTr.up, 
+                GameVariablesScript.Instance.orbSpeed);
+            
+            _transform.Translate(new Vector3(0,Mathf.Cos(_timeSpend) * 
+                                                Time.deltaTime/2,0),Space.Self);
+        }
+
+        private void OnDisable()
+        {
+            _timeSpend = 0.0f;
+            _transform.position = initialPosition;
+            _transform.rotation = initialRotation;
+        }
     }
 }
