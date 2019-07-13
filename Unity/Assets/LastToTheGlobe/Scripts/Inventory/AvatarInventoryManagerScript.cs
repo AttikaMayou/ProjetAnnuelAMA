de614c463788a95ec368967a34be9021dc81ad85
Attika <attika.mayou@gmail.com>
2019-07-13 17:01:08 +0200

    Merge stuff

--- Unity/Assets/LastToTheGlobe/Scripts/Inventory/AvatarInventoryManagerScript.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Inventory/AvatarInventoryManagerScript.cs
@@ -1,45 +1,38 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using LastToTheGlobe.Scripts.Avatar;
-using LastToTheGlobe.Scripts.Inventory;
+﻿using LastToTheGlobe.Scripts.Avatar;
 using Photon.Pun;
 using UnityEngine;
 
 //Auteur : Abdallah
 
-public class AvatarInventoryManagerScript : MonoBehaviour
+namespace LastToTheGlobe.Scripts.Inventory
 {
     public class AvatarInventoryManagerScript : MonoBehaviour
-    {/*
-    public CharacterExposerScript selfExposer;
-
-    [SerializeField] private PhotonView inventoryPhotonView;
-
-    private ObjectScript objectToAdd;
-
-    // Start is called before the first frame update
-    void Start()
     {
-        selfExposer.PlayerInventory.SetActive(false);
-    }
+        public CharacterExposerScript selfExposer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Check if an item have been added
-        foreach (var item in selfExposer.InventoryExposer.playerSlot)
+        [SerializeField] private PhotonView inventoryPhotonView;
+
+        private ObjectScript _objectToAdd;
+
+        private void Awake()
         {
-            if (item.transform.childCount > 2 && !item.transform.GetChild(2).CompareTag("Untagged"))
+            selfExposer.PlayerInventory.SetActive(false);
+        }
+
+        private void Update()
+        {
+            //Check if an item have been added
+            foreach (var item in selfExposer.InventoryExposer.playerSlot)
             {
-                item.transform.GetChild(2).SetAsFirstSibling();
-                //Send RPC to add item
+                if (item.transform.childCount > 2 && !item.transform.GetChild(2).CompareTag("Untagged"))
+                {
+                    item.transform.GetChild(2).SetAsFirstSibling();
+                    //Send RPC to add item
                 
-                inventoryPhotonView.RPC("AddItemToInventory", RpcTarget.MasterClient, item.transform.GetChild(0).tag, selfExposer.Id);
+                    inventoryPhotonView.RPC("AddItemToInventory", RpcTarget.MasterClient, item.transform.GetChild(0).tag, selfExposer.Id);
                 
+                }
             }
         }
-        
     }
 }
