2a828723bc87a0379dcff94fe1db262e2055a495
Margot <gotgot07@hotmail.fr>
2019-07-14 15:48:59 +0200

    Joli sun

--- Unity/Assets/LastToTheGlobe/Scripts/Environment/SunController.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/Environment/SunController.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
-
+using LastToTheGlobe.Scripts.Management;
 //Auteur : Attika
 
 namespace LastToTheGlobe.Scripts.Environment
 {
     public class SunController : MonoBehaviour
     {
-        [SerializeField] private Vector3 startPos;
-        
+        public float t;
+
         private void Awake()
         {
-            gameObject.transform.position = startPos;
+            gameObject.transform.position = GameVariablesScript.Instance.startPos;
         }
         
+        private void Update()
+        {
+            t += Time.deltaTime / GameVariablesScript.Instance.timeToReachTarget;
+            transform.position = Vector3.Lerp(GameVariablesScript.Instance.startPos, GameVariablesScript.Instance.target, t);
+        }
+
+
         //TODO : add methods for sun control (UI feedback for players + movement of the object + lighting setup)
     }
 }
