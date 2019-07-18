c37cbcd6c82bc80cd18a9003bfff0a8be30eda60
Margot <gotgot07@hotmail.fr>
2019-07-18 19:02:14 +0200

    data player on planet

---
+++ Unity/Assets/LastToTheGlobe/Scripts/ScriptableObject/GetDataOnPlanet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class GetDataOnPlanet : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/Assets/LastToTheGlobe/Scripts/ScriptableObject/PlayerInPlanetTime.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerOnPlanet", menuName = "PlayerOnPlanet", order = 2)]
+public class PlayerInTime : ScriptableObject
+{
+    [SerializeField]
+    private List<PlanetTimeData> PlanetDataList;
+
+    public List<PlanetTimeData> publicPlanetDataList => this.PlanetDataList;
+
+    public void ResetData()
+    {
+        this.PlanetDataList.Clear();
+    }
+
+    //fonction de data à collecter
+    public void AddidPlanet(int IdPlanet)
+    {
+        this.PlanetDataList.Add(new PlanetTimeData() { IdPlanet = IdPlanet });
+    }
+    public void AddTimeEntryCollider(float EnterTime)
+    {
+        this.PlanetDataList.Add(new PlanetTimeData() {EnterTime = EnterTime });
+    }
+    public void AddTimeExitCollider(float ExitTime)
+    {
+        this.PlanetDataList.Add(new PlanetTimeData() { ExitTime = ExitTime });
+    }
+    public void AddnumberOfChest(int numberOfChest)
+    {
+        this.PlanetDataList.Add(new PlanetTimeData() { numberOfChest = numberOfChest });
+    }
+    public void AddtimeChestOpened(float timeChestOpened)
+    {
+        this.PlanetDataList.Add(new PlanetTimeData() { timeChestOpened = timeChestOpened });
+    }
+}
+
+//class de datas que l'on veut collecter
+[Serializable]
+public class PlanetTimeData
+{
+    public int IdPlanet;
+    public int numberOfChest;
+    public float EnterTime;
+    public float ExitTime;
+    public int numberOfPlayer;
+    public float timeChestOpened;
+}
+
+
