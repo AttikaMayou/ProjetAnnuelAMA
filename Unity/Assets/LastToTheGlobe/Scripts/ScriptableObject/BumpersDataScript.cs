f98245fee6062a3a5f8df0f37463595d47675010
Attika <attika.mayou@gmail.com>
2019-07-19 13:03:05 +0200

    WIP Bumpers Data

--- Unity/Assets/LastToTheGlobe/Scripts/ScriptableObject/BumpersDataScript.cs
+++ Unity/Assets/LastToTheGlobe/Scripts/ScriptableObject/BumpersDataScript.cs
@@ -1,16 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace LastToTheGlobe.Scripts.ScriptableObject
 {
     public class BumpersDataScript : MonoBehaviour
     {
-        
+        private List<BumpersData> _bumpersData = new List<BumpersData>();
+
+        public List<BumpersData> publicBumpersData => this._bumpersData;
+
+        public void ResetData()
+        {
+            this._bumpersData.Clear();
+        }
+
+        public void AddBumperData(int bumperId, Vector3 bumperPos)
+        {
+            this._bumpersData.Add(new BumpersData()
+            {
+                BumperId = bumperId,
+                BumperPos = bumperPos
+            });
+        }
     }
 
     public class BumpersData
     {
         public int BumperId;
         public Vector3 BumperPos;
-        public Vector3 KillPos;
+        //public Vector3 KillPos;
     }
 }
