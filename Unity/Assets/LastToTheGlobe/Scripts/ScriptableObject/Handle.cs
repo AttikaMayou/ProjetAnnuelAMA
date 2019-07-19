d6db5b113f97e7444d4e5cb29f7b276b391f438d
Margot <gotgot07@hotmail.fr>
2019-07-19 12:19:36 +0200

    essai désespéré d'avoir un Editor

---
+++ Unity/Assets/LastToTheGlobe/Scripts/ScriptableObject/Handle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class HandleExample : MonoBehaviour
+{
+    public float shieldArea = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
